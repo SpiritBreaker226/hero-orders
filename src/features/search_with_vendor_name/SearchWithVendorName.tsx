c2d1fb72043cc718bb5186869b1d4baebeb3a84e
Jason Stathopulos <jstathopulos@gmail.com>
2020-10-29 19:33:16 -0400

    Add Styles to Search With Vendor Name Component
    Add the Search With Vendor Name component styles to match the design and
    be a responsive design roughly.

--- src/features/search_with_vendor_name/SearchWithVendorName.tsx
+++ src/features/search_with_vendor_name/SearchWithVendorName.tsx
@@ -7,6 +7,8 @@ import {
   updateSearchWithVendorName,
 } from './searchWithVendorNameSlice'
 
+import styles from './SearchWithVendorName.module.scss'
+
 const SearchWithVendorName = () => {
   const dispatch = useDispatch()
   const { value, options } = useSelector(selectSearchWithVendorName)
@@ -19,29 +21,49 @@ const SearchWithVendorName = () => {
   }
 
   return (
-    <section>
-      <p>
-        <label htmlFor="searchWithVendorName">Suppliers</label>
-      </p>
-      <select
-        id="searchWithVendorName"
-        name="searchWithVendorName"
-        data-testid="searchWithVendorName"
-        onChange={handleChangeSearchBox}
-        value={value}
-      >
-        {options.map((option) => {
-          return (
-            <option key={option.value} value={option.value}>
-              {option.label}
-            </option>
-          )
-        })}
-      </select>
+    <section className={styles.searchWithVendorName}>
+      <div className={styles.searchWithVendorNameForum}>
+        <div className={styles.fieldLabelItem}>
+          <label className={styles.fieldLabel} htmlFor="searchWithVendorName">
+            Suppliers
+          </label>
+        </div>
 
-      <button onClick={handleClickResetFilter} data-testid="searchResetButton">
-        X Reset Filter
-      </button>
+        <div className={styles.fieldItem}>
+          <div className={styles.fieldLabelItemForDesktop}>
+            <label className={styles.fieldLabel} htmlFor="searchWithVendorName">
+              Suppliers
+            </label>
+          </div>
+
+          <select
+            id="searchWithVendorName"
+            name="searchWithVendorName"
+            data-testid="searchWithVendorName"
+            onChange={handleChangeSearchBox}
+            value={value}
+            className={styles.select}
+          >
+            {options.map((option) => {
+              return (
+                <option key={option.value} value={option.value}>
+                  {option.label}
+                </option>
+              )
+            })}
+          </select>
+        </div>
+
+        <div className={styles.fieldItem}>
+          <button
+            onClick={handleClickResetFilter}
+            data-testid="searchResetButton"
+            className={styles.button}
+          >
+            X Reset Filter
+          </button>
+        </div>
+      </div>
     </section>
   )
 }
